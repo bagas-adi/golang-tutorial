4bd9baa2d80cf11f6fa96cd102c399917124f600
Bagas <bagasap90@gmail.com>
2021-01-24 20:30:35 +0700

    update

---
+++ ch1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var card string
+
+// var card string = "Ace of Spades"
+
+func main() {
+	card = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	fmt.Println(card)
+}
---
+++ ch1/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// inisialisasi
+var card string
+
+// deklarasi variable
+// var card string = "Ace of Spades"
+
+func main() {
+	// assign value to a variablem after it declared or initialized
+	card = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	fmt.Println(card)
+}
